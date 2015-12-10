b155d7905c34afcb3ef9ce0fcc99c3c18c4f1313
Hieu Tran <trunghieu.tran138@gmail.com>
2015-12-10 15:55:04 +0200

    feedback all cities

--- kitchen/oven/bigboss/bigboss_urls.py
+++ kitchen/oven/bigboss/bigboss_urls.py
@@ -8,4 +8,5 @@ urlpatterns = patterns(
     url(r'^create', bigboss_views.AssignmentView.as_view()),
     url(r'^assignment/(?P<id>[0-9]+)$', bigboss_views.AssignmentDetailView.as_view()),
     url(r'^assignment', bigboss_views.AssignmentAllView.as_view()),
+    url(r'^feedback', bigboss_views.FeedbackRequestView.as_view()),
 )
